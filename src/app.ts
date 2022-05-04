72452527df810897c70b03ec3ede4b429d1b52af
Mika Greif <mika.greif@greenbone.net>
2022-05-04 21:14:43 +0200

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
-
+import * as cors from 'cors'
 import indexRouter from './routes/index';
 import userRouter from './routes/user.route'
 import { errorHandler } from './errorHandler';
@@ -9,6 +9,11 @@ import { loggerMiddleware } from './config/logger';
 const app = express();
 
 app.use(express.json());
+const corsOptions = {
+    origin: 'http://frontend.greif.me'
+}
+
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(loggerMiddleware)
